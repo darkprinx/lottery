dcd7bd1b93671f1903981b806256861040561b4a
tushar <masud.tushar@cefalo.com>
2022-12-23 13:52:06 +0600

    Add scheduler to close lottery every midnight

---
+++ core/scheduler.py
@@ -0,0 +1,21 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from core.scheduled_tasks import close_active_lottery
+
+"""This task will be triggered at 12.01 AM every day on UTC time"""
+task_list_config = [
+    {
+        'func': close_active_lottery,
+        'trigger': 'cron',
+        'hour': 0,
+        'minute': 1,
+        'timezone': 'UTC'
+    },
+]
+
+
+def start():
+    scheduler = BackgroundScheduler()
+    for task_config in task_list_config:
+        scheduler.add_job(**task_config)
+    scheduler.start()
--- lottery_events/apps.py
+++ lottery_events/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class LotteryEventsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'lottery_events'
+
+    def ready(self):
+        from core import scheduler
+        scheduler.start()
