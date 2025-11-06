6bce4655ecacd06252e9104cd4100710f1f63ad4
darkprinx <rctushar07@gmail.com>
2025-11-06 12:24:09 +0600

    feat: enhance Docker Python action with output greeting functionality

--- .github/actions/docker-python-action/main.py
+++ .github/actions/docker-python-action/main.py
@@ -2,8 +2,8 @@ import os
 
 def main():
     name = os.environ.get('INPUT_NAME', 'World')
-    print(f'Hello {name} from Python Docker Action!')
-    print(f'::set-output name=greeting::Hello {name} from Python Docker Action!')
+    print(f"👋 Hello from Python, {name}!")
+    print(f"echo 'greeting=Greetings from {name}' >> $GITHUB_OUTPUT")
 
 if __name__ == "__main__":
     main()
