55994969d69487462854fe5cf325bf2a2bd7b395
darkprinx <rctushar07@gmail.com>
2025-11-06 13:02:51 +0600

    feat: update greeting functionality to output to GITHUB_OUTPUT

--- .github/actions/docker-python-action/main.py
+++ .github/actions/docker-python-action/main.py
@@ -1,9 +1,10 @@
 import os
 
 def main():
-    name = os.environ.get('INPUT_NAME', 'World')
+    name = os.environ.get('INPUT_NAME')
     print(f"👋 Hello from Python, {name}!")
-    print(f"echo 'greeting=Greetings from {name}' >> $GITHUB_OUTPUT")
+    with open(os.environ["GITHUB_OUTPUT"], "a") as gh_out:
+        print(f"greeting=Hello from Python, {name}!", file=gh_out)
 
 if __name__ == "__main__":
     main()
