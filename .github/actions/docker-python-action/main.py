f9511bb7b2f75c066b35bf4c1d8de46e457a601a
darkprinx <rctushar07@gmail.com>
2025-11-13 13:29:59 +0600

    feat: update action authors and add Code Quality Checker workflow

--- .github/actions/docker-python-action/main.py
+++ .github/actions/docker-python-action/main.py
@@ -1,10 +1,12 @@
 import os
 
+
 def main():
-    name = os.environ.get('INPUT_NAME')
+    name = os.environ.get("INPUT_NAME")
     print(f"👋 Hello from Python, {name}!")
     with open(os.environ["GITHUB_OUTPUT"], "a") as gh_out:
         print(f"greeting=Hello from Python, {name}!", file=gh_out)
+
 
 if __name__ == "__main__":
     main()
